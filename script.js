6e9b702a9838005a0b4a75462629872ea777f945
miqdad <miqdad.shaldin@gmail.com>
2025-05-30 20:56:21 +0700

    add for loop to catch clicked button, and add value to input display

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ const display = document.querySelector(".display");
 
 const buttons = document.querySelectorAll("button");
 
-console.log(buttons);
+function addValue(val) {
+  display.value += val;
+}
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    let val = buttons[i].innerHTML;
+    if (val !== "=" && val !== "AC") {
+      addValue(val);
+    }
+  });
+}
